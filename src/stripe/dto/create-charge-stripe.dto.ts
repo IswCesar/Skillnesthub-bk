6dd64cd5780b143729c2ef9c1c1f7e112984730a
Cesar <cardona.cesar@outlook.es>
2023-11-22 12:38:58 -0600

    feat: Added make charge 3

--- src/stripe/dto/create-charge-stripe.dto.ts
+++ src/stripe/dto/create-charge-stripe.dto.ts
@@ -2,6 +2,10 @@ import { IsNumber, IsString } from 'class-validator';
 export class CreateChargeStripeDto {
   @IsNumber()
   price: number;
+
+  @IsString()
   description: string;
+
+  @IsString()
   source: string;
 }
